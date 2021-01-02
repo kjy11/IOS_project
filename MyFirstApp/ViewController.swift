4174f2167f123f84a3f7445859642576f88cfbf2
jykim <>
2021-01-02 15:44:20 +0900

    initial commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by 김재연 on 2021/01/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+//    @IBOutlet weak var numLabel: UILabel!
+//    var num = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+//    @IBAction func numUp(_ sender: UIButton) {
+//        num = num + 1
+//        numLabel.text = String(num)
+//        showToast(message: "숫자 증가" + String(num))
+//    }
+//
+//    func showToast(message : String, font: UIFont = UIFont.systemFont(ofSize: 14.0)) {
+//        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+//        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+//        toastLabel.textColor = UIColor.white
+//        toastLabel.font = font
+//        toastLabel.textAlignment = .center
+//        toastLabel.text = message
+//        toastLabel.alpha = 1.0
+//        toastLabel.layer.cornerRadius = 10
+//        toastLabel.clipsToBounds = true
+//        self.view.addSubview(toastLabel)
+//        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations:
+//                        {
+//                            toastLabel.alpha = 0.0
+//                        },
+//                       completion: {
+//                        (isCompleted) in toastLabel.removeFromSuperview()
+//                       }
+//        )
+//
+//    }
+
+    
+}
+
